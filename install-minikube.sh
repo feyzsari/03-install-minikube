8bd4fc32d815104bed9bf8262a34e2a6d0d3fd3b
Feyz Sari <101112971+feyzsari@users.noreply.github.com>
2022-03-12 16:20:08 +0000

    docker group creation added

--- install-minikube.sh
+++ install-minikube.sh
@@ -41,19 +41,22 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 # Install kubectl
- sudo snap install kubectl 
+# sudo snap install kubectl 
 
 
-#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-#curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-#echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
-#sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-#kubectl version --client
- 
-# Install  Minikube
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(<kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+# user settings
 sudo su
 usermod -aG sudo ubuntu
 su - ubuntu
+sudo usermod -aG docker $USER && newgrp docker
+
+# Install  Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
 sudo dpkg -i minikube_latest_amd64.deb
 minikube start
